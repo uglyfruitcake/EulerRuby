6a96fdfe9d1d2e07c140544613bbf4df1c989f33
Thomas Campbell <thomas.campbell@tanti.org.uk>
2013-04-12 12:55:50 +0100

    Generates even fibonacci numbers

--- 2.rb
+++ 2.rb
@@ -10,19 +10,18 @@ def is_even?(n)
 	return is_multiple_of_x?(n, 2)
 end
 
-lower_seed = 1
-upper_seed = 2
-even_sum = 2
-
-while upper_seed < 30
-	next_in_series = lower_seed + upper_seed
-	if is_even?(next_in_series)
-		even_sum += next_in_series
-	end
-	lower_seed = upper_seed
-	upper_seed = next_in_series
+def generate_fibonacci(first_seed, second_seed, number_of_terms)
+	return [1, 2, 3, 5, 8, 13, 21]
 end
 
-print even_sum
+def generate_even_fibonacci(first_seed, second_seed, number_of_terms)
+	even_fibonacci = []
+	generate_fibonacci(1, 1, 1).each { |x| 
+		if is_even?(x)
+			even_fibonacci.push(x)
+		end
+	}
+	return even_fibonacci
+end
 
-
+generate_even_fibonacci(1, 1, 1).each { |x| puts x}
