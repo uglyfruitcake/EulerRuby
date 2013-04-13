cb1d58ce5c130cb0fc82090b79a127d3880ec856
Thomas Campbell <thomas.campbell@tanti.org.uk>
2013-04-13 12:12:36 +0100

    Creates useful changes

---
+++ Useful_Methods.rb
@@ -0,0 +1,13 @@
+module Useful_Methods
+	def is_multiple_of_x?(n, x)
+		if n % x == 0
+			return true
+		else
+			return false
+		end
+	end
+
+	def is_even?(n)
+		return is_multiple_of_x?(n, 2)
+	end
+end
