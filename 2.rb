4577707f897083bc0819296606ab4da36cfbb903
Thomas Campbell <thomas.campbell@tanti.org.uk>
2013-04-15 11:38:48 +0100

    Adds fibonacci's to module

--- 2.rb
+++ 2.rb
@@ -2,19 +2,9 @@
 
 require_relative 'useful_methods'
 
-def generate_fibonacci(first_seed, second_seed, max_value)
-	array = [first_seed, second_seed]
-	while first_seed + second_seed < max_value
-		array.push(first_seed + second_seed)
-		second_seed = first_seed + second_seed
-		first_seed = second_seed - first_seed
-	end
-	return array
-end
-
 def generate_even_fibonacci(first_seed, second_seed, max_value)
 	even_fibonacci = []
-	generate_fibonacci(first_seed, second_seed, max_value).each { |x| 
+	Useful_Methods.generate_fibonacci(first_seed, second_seed, max_value).each { |x| 
 		if Useful_Methods.is_even?(x)
 			even_fibonacci.push(x)
 		end
--- Useful_Methods.rb
+++ Useful_Methods.rb
@@ -10,6 +10,16 @@ module Useful_Methods
 	def is_even?(n)
 		return is_multiple_of_x?(n, 2)
 	end
+
+	def generate_fibonacci(first_seed, second_seed, max_value)
+		array = [first_seed, second_seed]
+		while first_seed + second_seed < max_value
+			array.push(first_seed + second_seed)
+			second_seed = first_seed + second_seed
+			first_seed = second_seed - first_seed
+		end
+		return array
+	end
 end
 
 include Useful_Methods
